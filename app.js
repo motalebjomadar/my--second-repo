b9ab9034172d9dbc7560289aaaa3e60ce5208256
motalebjomadar <motalabjomadar@gmail.com>
2023-12-26 00:28:59 +0800

    Add element when click on button then remove element when click on created item with js dom

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const addBtn = document.getElementById('btn');
+
+// Create container tag with js dom
+addBtn.addEventListener('click', function() {
+    const item = document.createElement('li');
+    item.classList.add('list-item');
+    item.innerText = 'item added';
+    document.body.appendChild(item);
+
+
+    item.addEventListener('click', function (event) {
+    
+        event.target.parentNode.removeChild(event.target);
+    });
+})
